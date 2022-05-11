3f5eeecb368da146b03ac1b069231e676a5c8bfa
NOIR <bakarysylla58@gmail.com>
2022-05-11 22:57:30 +0000

    Ajoutez des fichiers projet.

---
+++ codeFirst/Models/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace codeFirst.Models
+{
+    public class Client
+    {
+        [Key]
+        public int IdClient { get; set; }
+
+
+        [Display(Name = "Nom du client client"), MaxLength(80, ErrorMessage = "*"), Required(ErrorMessage = "saisir le le nom svp  pour avancer")]
+        public string NomClient { get; set; }
+
+        [Display(Name = "Prenom du client"), MaxLength(80, ErrorMessage = "*"), Required(ErrorMessage = "saisir le le PrenomClient svp  pour avancer")]
+        public string PrenomClient { get; set; }
+
+        [Display(Name = "Sa fonction"), MaxLength(80, ErrorMessage = "*"), Required(ErrorMessage = "saisir le le Fonction svp  pour avancer")]
+        public string Fonction { get; set; }
+
+        [Display(Name = "Son salaire"), Required(ErrorMessage = "saisir le Salair svp")]
+        public long Salair { get; set; }
+
+      
+    }
+}
---
+++ codeFirst/Models/ClientContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace codeFirst.Models
+{
+    public class ClientContext:DbContext
+    {
+        public ClientContext() : base("Conn") { }
+        public DbSet<Client> clients { get; set; }
+    }
+}
